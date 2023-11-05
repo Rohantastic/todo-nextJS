bdd4b387a468f8663cc63bfe5698240c2ecd50b1
rohantastic <rohantastic@outlook.com>
2023-11-05 17:35:23 +0530

    update

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import "./Layout.css";
-
-
-
 const Layout = ({ children }) => (
   <div>
     <header>Todo App</header>
     <main>{children}</main>
-    <footer>© 2023</footer>
+    <footer>© 2023, created by ROhan!!</footer>
   </div>
 );
 
